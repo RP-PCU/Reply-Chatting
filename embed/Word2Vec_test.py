8a623af514c156f27edada58e2f109a4fafcf841
HyoJun-Ahn <wnsgy79@gmail.com>
2021-10-23 18:02:46 +0900

    embed

--- embed/Word2Vec_test.py
+++ embed/Word2Vec_test.py
@@ -1,13 +1,4 @@
 # %%
-import urllib.request
-import zipfile
-from lxml import etree
-import re
-from nltk.tokenize import word_tokenize, sent_tokenize
+from gensim.models import Word2Vec
+from gensim.models import KeyedVectors
 # %%
-urllib.request.urlretrieve("https://raw.githubusercontent.com/GaoleMeng/RNN-and-FFNN-textClassification/master/ted_en-20160408.xml", filename="ted_en-20160408.xml")
-# %%
-targetXML = open('ted_en-20160408.xml', 'r', encoding='UTF8')
-target_text = etree.parse(targetXML)
-# %%
-parse_text = '\n'.join(target_text.xpath('//content/text()'))
