a049e676c051751479c51341c1e8b4e8951ab44c
HyoJun-Ahn <wnsgy79@gmail.com>
2021-10-23 18:02:27 +0900

    embed

--- embed/Word2Vec_test.py
+++ embed/Word2Vec_test.py
@@ -4,3 +4,10 @@ import zipfile
 from lxml import etree
 import re
 from nltk.tokenize import word_tokenize, sent_tokenize
+# %%
+urllib.request.urlretrieve("https://raw.githubusercontent.com/GaoleMeng/RNN-and-FFNN-textClassification/master/ted_en-20160408.xml", filename="ted_en-20160408.xml")
+# %%
+targetXML = open('ted_en-20160408.xml', 'r', encoding='UTF8')
+target_text = etree.parse(targetXML)
+# %%
+parse_text = '\n'.join(target_text.xpath('//content/text()'))
