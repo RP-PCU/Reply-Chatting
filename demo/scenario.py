481a74c1359d1ca77bad917f94ab640c10eb6ceb
Hyojun Ahn <wnsgy79@gmail.com>
2021-11-21 19:22:46 +0900

    web

--- demo/scenario.py
+++ demo/scenario.py
@@ -3,22 +3,23 @@ from Reply_Chatting.app import Scenario
 from kocrawl.map import MapCrawler
 from KOCRAWL.kocrawl.uni import UniCrawler
 
-# 장소
-Place = Scenario(
-    intent='information',
-    api=MapCrawler().request,
+
+# 건물
+university = Scenario(
+    intent='university',
+    api=UniCrawler().request,
     scenario={
         'PLACE': []
     }
 )
 
-university = Scenario(
-    intent='university',
-    api=UniCrawler().request,
+# 맛집
+restaurnt = Scenario(
+    intent='restaurnt',
+    api=MapCrawler().request,
     scenario={
         'LOCATION': [],
-        'PLACE': ['학교건물']
+        'PLACE': ['맛집']
     }
 )
-
 
