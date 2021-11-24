3748b0e33a9bafbd014846b34954bdc1d316058e
HyoJun-A <wnsgy79@gmail.com>
2021-11-24 22:02:48 +0900

    chat

---
+++ KOCRAWL/kocrawl/answerer/university_answerer.py
@@ -0,0 +1,25 @@
+from kocrawl.answerer.base_answerer import BaseAnswerer
+
+#TODO배재대학교 건물 출력 포맷
+class UniAnswerer(BaseAnswerer):
+    
+    def Uni_form(self, location: str,place: str, result: dict) -> str:
+        """
+        배재대학교 건물  출력 포맷 
+        
+        :param location: 학교
+        :param place: 장소
+        :param result: 데이터 딕셔너리
+        :return: 출력 메시지
+        """
+        
+        msg = self.uni_init_format(palce=place)
+        msg += '{location} 의'
+
+        msg = self._add_msg_from_dict(result, 'category', msg, '{category}')
+        msg = self._add_msg_from_dict(result, 'address', msg, '주소는 {address}입니다.')
+        msg = self._add_msg_from_dict(result, 'thumUrl', msg, '> 사진보기 : {thumUrl}')
+        msg = msg.format(location=location, category=result['category'],address=result['address'], 
+                         thumUrl=result['thumUrl'])
+
+        return msg
