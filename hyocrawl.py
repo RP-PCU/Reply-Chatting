b832109ddd2e3b8244b3919a66dbe2faf9095bd0
HyoJun-A <wnsgy79@gmail.com>
2021-11-29 20:18:35 +0900

    crawl

--- hyocrawl.py
+++ hyocrawl.py
@@ -1,5 +1,6 @@
 # %%
 from os import terminal_size
+from numpy.lib.shape_base import column_stack
 import pandas as pd
 from pandas._config.config import reset_option
 from pandas.core.frame import DataFrame 
@@ -7,7 +8,7 @@ from pandas.core.frame import DataFrame
 
 # %%
 def crawl(user):
-    
+    #
     data = pd.read_csv(r"C:\Reply-Chatting\test.csv", encoding='utf-8')
     # data 데이터프레임 형식으로 변환 
     df = pd.DataFrame(data)
@@ -28,13 +29,11 @@ df.rename(columns={"성명":"name"}, inplace=True)
 answer = df["name"] == user
 result = df[answer]
 print(result)
-
 # %%
 df_t = pd.read_csv('professor_info.txt', encoding = 'euc-kr', sep=" ") 
 df = pd.DataFrame(df_t)
 df.rename(columns={"성명":"name"}, inplace=True)
 print(df.columns)
-
 # %%
 data = pd.read_csv(r"C:\Reply-Chatting\test.csv", encoding='utf-8')
     # data 데이터프레임 형식으로 변환 
@@ -47,11 +46,15 @@ with open('professor_info.txt', "r") as f:
     lines = f.readlines()
 with open('professor_info.txt', "w") as f:
     for line in lines:
-        if line.strip("\n") != "테이블 캡션":
+        if line.strip("\n") != "":
             f.write(line)
             
 # %%
-cd = ["김창수", "정회경"]
-for i in cd:
-    print(i)
-    
+df_t = pd.read_csv('professor_info.txt', encoding = 'euc-kr', sep=" ")
+print(df_t)
+df = pd.DataFrame(df_t)
+df.rename(columns={"성명":"name"}, inplace=True)
+print(df.columns)
+answer = df["name"] == "정회경"
+result = df[answer]
+print(result)
